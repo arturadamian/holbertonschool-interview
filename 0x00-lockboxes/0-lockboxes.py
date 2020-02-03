833fddaba4a87d11b2234e0dc4799294a24972b9
arturadamian <arturadamian@gmail.com>
2020-02-03 13:02:28 -0800

    clean up the code

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 
+""" You have n number of locked boxes in front of you.
+    Each box is numbered sequentially from 0 to n - 1 and
+    each box may contain keys to the other boxes.
+    Write a method that determines if all the boxes can be opened.
 
-'''
-
-You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes. Write a method that determines if all the boxes can be opened.
-
-Prototype: def canUnlockAll(boxes)
-boxes is a list of lists
-A key with the same number as a box opens that box
-You can assume all keys will be positive integers
-The first box boxes[0] is unlocked
-Return True if all boxes can be opened, else return False'''
-
-boxes1 = [[1], [2], [3], [4], []]
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-
+    Prototype: def canUnlockAll(boxes)
+    boxes is a list of lists
+    A key with the same number as a box opens that box
+    You can assume all keys will be positive integers
+    The first box boxes[0] is unlocked
+    Return True if all boxes can be opened, else return False
+"""
 
 def canUnlockAll(boxes):
     index = 0
-    keys = list(range(1,len(boxes))) 
+    keys = list(range(1,len(boxes)))
     shared_box = []
     return canUnlock(boxes, index, keys, shared_box)
 
@@ -37,7 +32,4 @@ def canUnlock(boxes, index, keys, shared_box):
         if key == len(shared_box) - 1:
             return False
     return canUnlock(boxes, index, keys, shared_box)
-if __name__ == "__main__":
-    x = canUnlockAll(boxes3)
-    print(x)
 
