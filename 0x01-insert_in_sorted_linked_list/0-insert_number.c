31c5184f3e7f11bcedcb064f4dafdb43ce510a2f
arturadamian <arturadamian@gmail.com>
2020-02-10 20:22:49 +0000

    check for isdigit

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -17,6 +17,8 @@ listint_t *insert_node(listint_t **head, int number)
 
 		if (!head)
 			return (NULL);
+        if (!isdigit(number))
+            return (NULL)
 		new = malloc(sizeof(*new));
 		if (!new)
 			return (NULL);
