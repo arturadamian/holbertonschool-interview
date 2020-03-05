b04f3ad28a45de355edb3af163985ce63c5a6ed4
arturadamian <arturadamian@gmail.com>
2020-03-05 15:18:43 -0800

    rewrite using recursion

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,40 +1,43 @@
 #include"lists.h"
 
+
+/**
+ * is_palindrome - checks if the linked list is a palindrom
+ * with the help of the recursion function
+ * @head: pointer to the list
+ *
+ * Return: 0 - false or 1 - true
+ **/
 int is_palindrome(listint_t **head)
 {
-		listint_t *cur;
-		unsigned int len;
-		int idx = 1;
-		int flag = 0;
+				listint_t *tail;
+		unsigned int idx;
 
+		tail = *head;
+		idx = 0;
 		if (!head)
-				return (0);
+			return (0);
 		if (!*head)
-				return (1);
-		cur = *head;
-		len = 1;
-		while (cur->next)
-		{
-				len++;
-				cur = cur->next;
-		}
-		int arr[len / 2];
+			return (1);
+		return (recursion(head, tail));
+}
 
-		cur = *head;
-		while (cur->next)
-		{
-				if (flag == 0)
-						arr[idx] = cur->n;
-				if (idx == len / 2)
-						flag = 1;
-				cur = cur->next;
-				if (flag == 1 && cur->n == arr[idx])
-						idx--;
-				else if (flag == 0)
-						 idx++;
-				else
-						return (0);
-		}
-		return (1);
+/**
+ * recursion - on the way back checking elements with another pointer
+ * that starts moving from the head,
+ * @head: pointer to the list
+ * @tail: pointer to the head of the list
+ *
+ * Return: 0 - false or 1 - true
+ **/
+int recursion(listint_t **head, listint_t *tail)
+{
+		if (!tail)
+			return (1);
+		if (!recursion(head, tail->next))
+			return (0);
+		int answer = (tail->n == (*head)->n);
+		*head = (*head)->next;
+		return (answer);
 }
 
