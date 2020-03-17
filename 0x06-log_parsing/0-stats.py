4549fe1ad43e9bbe86cde59553f168e497b6272b
arturadamian <arturadamian@gmail.com>
2020-03-16 23:59:01 -0700

    incorrect putting codes in dictionary

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,12 +7,19 @@ if __name__ == '__main__':
 
     import sys
     
+    def printer(size, d):
+        """Printer function"""
+        a = sorted(d.keys())
+        print("File size: {:d}".format(size))
+        for i in a:
+            if d[i] != 0:
+                print("{}: {}".format(i, d[i]))
     def printMetrix(fileSize, statusCodes):
         """
             prints the metrix
         """
 
-        print("File size: {}".format(fileSize))
+        print("File size: {:d}".format(fileSize))
         for status, count in sorted(statusCodes.items()):
             if count:
                 print("{}: {}".format(status, count))
@@ -20,17 +27,27 @@ if __name__ == '__main__':
     k = 0
     fileSize = 0
     statusCodes = {}
+    statusCodes = { "200": 0,
+                    "301": 0,
+                    "400": 0,
+                    "401": 0,
+                    "403": 0,
+                    "404": 0,
+                    "405": 0,
+                    "500": 0
+                    }
     try:
         for line in sys.stdin:
             k += 1
             data = line.split()
             try:
-                fileSize += int(data[-1])
                 code = data[-2]
                 if code in statusCodes:
                     statusCodes[code] += 1
-                else:
-                    statusCodes[code] = 0
+            except BaseException:
+                pass
+            try:
+                fileSize += int(data[-1])
             except BaseException:
                 pass
             if k % 10 == 0:
