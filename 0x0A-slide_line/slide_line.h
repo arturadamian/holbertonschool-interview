de6d03e6828f230ef093bea1bdd5e18f3d38581e
arturadamian <arturadamian@gmail.com>
2020-04-15 23:50:11 -0700

    slide_line.h

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,11 +1,11 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef SLIDE_LINE_H
+#define SLIDE_LINE_H
 
 #include <stdlib.h>
 #include <stdio.h>
 
-#define SLIDE_LEFT  ('\074')
-#define SLIDE_RIGHT  ('\076')
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT 2
 
 
 int slide_line(int *line, size_t size, int direction);
