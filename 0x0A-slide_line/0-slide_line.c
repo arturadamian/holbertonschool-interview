520e6a907f51008b03c1217660ffcba103808c91
arturadamian <arturadamian@gmail.com>
2020-04-19 22:48:59 -0700

    rewrote the code

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,94 +1,100 @@
 #include "slide_line.h"
 
-/**
- * swap - swaps two integers
- * @a: pointer to an int
- * @b: pointer to an int
- */
-void swap(int *a, int *b)
-{
-	int temp = *a;
-
-	*a = *b;
-	*b = temp;
-}
 
 /**
- * slide_left - slides an array of integers to the left,
- * skipping all the zeros in between.
- * @line: pointer to an array of integers.
- * @size: size of line array
- */
-void slide_left(int *line, size_t size)
+ * reverse - reverse the array
+ * @line: pointer to the array
+ * @size: size of the array
+ **/
+void reverse(int *line, size_t size)
 {
-	size_t i, p = 0;
+	size_t a, b;
 
-	for (i = 0; i < size && p < size; i++)
+	a = 0;
+	b = size - 1;
+	while (a < b)
 	{
-		while (line[p] == 0 && p < size && p + 1 < size)
-			p++;
-		if (line[i] == 0)
-			swap(&line[p], &line[i]);
-		p++;
+		int temp = line[a];
+
+		line[a] = line[b];
+		line[b] = temp;
+		a++;
+		b--;
 	}
 }
 
 /**
- * slide_right - slides an array of integers to the right,
- * skipping all the zeros in between.
- * @line: pointer to an array of integers.
- * @size: size of line array
- */
-void slide_right(int *line, size_t size)
+ * slide_zero - moves zeros
+ * @line: pointer to the array
+ * @size: size of the array
+ **/
+void slide_zero(int *line, size_t size)
 {
-	size_t i, p = size - 1;
+	size_t x;
+	size_t zero = 0;
 
-	for (i = size - 1; (int) i >= 0 && (int) p >= 0; i--)
+	x = 0;
+	while (x < size)
 	{
-		while (line[p] == 0 && (int) p > 0)
-			p--;
-		if (line[i] == 0)
-			swap(&line[p], &line[i]);
-		p--;
+		if (line[x] == 0)
+			zero++;
+		else if (zero)
+			line[x - zero] = line[x];
+		x++;
+	}
+	while (zero > 0)
+	{
+		line[x - 1] = 0;
+		zero--;
+		x--;
 	}
 }
 
 /**
- * slide_line - slides and merges an array of integers
- * @line: points to an array of integers
- * @size: number of elements of array
- * @direction: SLIDE_LEFT, SLIDE_RIGHT
- * Return: 1 upon success, or 0 upon failure.
- */
+ * slide_non_zero - moves non zero ints
+ * @line: pointer to the array
+ * @size: size of the array
+ **/
+void slide_non_zero(int *line, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (line[i] == line[i + 1])
+		{
+			line[i] *= 2;
+			line[i + 1] = 0;
+		}
+		i += 2;
+	}
+}
+
+/**
+ * slide_line - slides the array
+ * @line: pointer to the array
+ * @size: size of the array
+ *
+ * Return: 1 if success or 0
+ **/
 int slide_line(int *line, size_t size, int direction)
 {
-	size_t i = 0;
 
 	if (direction == SLIDE_LEFT)
 	{
-		slide_left(line, size);
-		for (i = 0; i < size; i++)
-		{
-			if (line[i] == line[i + 1])
-			{
-				line[i] = line[i] + line[i + 1];
-				line[i + 1] = 0;
-			}
-		}
-		slide_left(line, size);
+		slide_zero(line, size);
+		slide_non_zero(line, size);
+		slide_zero(line, size);
 		return (1);
-	} else if (direction == SLIDE_RIGHT)
+	}
+	else if (direction == SLIDE_RIGHT)
 	{
-		slide_right(line, size);
-		for (i = size - 1; (int) i >= 0; i--)
-		{
-			if (line[i] == line[i - 1])
-			{
-				line[i] = line[i] + line[i - 1];
-				line[i - 1] = 0;
-			}
-		}
-		slide_right(line, size);
+		reverse(line, size);
+		slide_zero(line, size);
+		slide_non_zero(line, size);
+		slide_zero(line, size);
+		reverse(line, size);
 		return (1);
 	}
 	return (0);
