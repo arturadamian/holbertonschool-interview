f9857008991dacaff89a158fb1362049c967d245
arturadamian <arturadamian@gmail.com>
2020-05-31 19:48:10 -0700

    initial commit

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', function (err, res, body) {
+  if (err) throw err;
+  for (const actor of JSON.parse(body).characters) {
+    request(actor, function (err, res, body) {
+      if (err) throw err;
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
