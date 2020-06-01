b0a34b5deca7e537f08621002fbbdfb9f2a85da3
arturadamian <arturadamian@gmail.com>
2020-05-31 19:49:21 -0700

    the endpoint

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', function (err, res, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
   for (const actor of JSON.parse(body).characters) {
     request(actor, function (err, res, body) {
