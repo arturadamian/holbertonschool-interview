95efeb0a221f646478db1eaee528562e5ffa05f2
arturadamian <arturadamian@gmail.com>
2020-07-30 00:29:13 +0000

    initial commit

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* check_cycle - checks if alinked list has a cycle
+* @head: pointer to a linked list
+*
+* Return: 0 if false or 1 if true
+**/
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tortuga = head;
+	listint_t *rabbit = head;
+
+	if (!head)
+		return (NULL);
+	while (tortuga->next && rabbit->next->next)
+	{
+		tortuga = tortuga->next;
+		rabbit = rabbit->next->next;
+		if (tortuga == rabbit)
+			return (tortuga);
+	}
+	return (NULL);
+}
