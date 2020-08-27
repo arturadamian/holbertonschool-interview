8e339921acc104279830438961cf6e9e47d66bcf
arturadamian <arturadamian@gmail.com>
2020-08-27 14:41:02 -0700

    add documentation

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed
+    to meet a given amount total
+"""
 
 
 def makeChange(coins, total):
+    """
+        return the fewest number of coins
+        0 if total less than 1
+        or -1 if it is impossible
+    """
     if total < 1:
         return 0
     MAX = float('inf')
@@ -13,7 +23,3 @@ def makeChange(coins, total):
                 arr[i] = min(arr[i], arr[i - c] + 1)
 
     return arr[i] if arr[i] != MAX else -1
-
-# if __name__=='__main__':
-#     print(makeChange([1, 2, 25], 37))
-#     print(makeChange([1256, 54, 48, 16, 102], 1453))
